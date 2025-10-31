85d31e382c0bf1a1ff0554429728f413f3a6884e
venkata-hari <xprogrammer.me@gmail.com>
2025-10-31 16:32:33 +0530

    reset password api implemented

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({origin: ['http://localhost:5173', 'http://localhost:5174']})
   app.setGlobalPrefix('api')
   app.useGlobalPipes(new ValidationPipe({whitelist:true,transform:true,forbidNonWhitelisted:true}))
   await app.listen(process.env.PORT ?? 3000);
